fc64d1f1539058fc77f74d5de78294c873ccc76e
SaadiDK-003 <pcremote059@gmail.com>
2024-03-16 18:35:24 +0500

    mail work

--- forgetPassword.php
+++ forgetPassword.php
@@ -7,91 +7,111 @@ $msg = '';
 $pageTitle = 'Forget Password';
 
 ?>
+
 <body>
 
 
-<div class="container">
-    <div class="row vh-100 align-items-center justify-content-center">
-        <div class="col-lg-4 col-md-6">
-            <h5 class="text-center text-danger mb-3 font-weight-bold"><?=$msg?></h5>
-            <h3 class="text-center mb-3 font-weight-bold"><?=$pageTitle?></h3>
-            <form id="login-form" action="submit_req.php" method="post">
+    <div class="container">
+        <div class="row vh-100 align-items-center justify-content-center">
+            <div class="col-lg-4 col-md-6">
+                <h5 id="response_msg" class="text-center mb-3 font-weight-bold"></h5>
+                <h3 class="text-center mb-3 font-weight-bold"><?= $pageTitle ?></h3>
+                <form id="reset-form">
+                    <div class="row">
+                        <div class="col-12 mb-3">
+                            <label for="email">Email</label>
+                            <input type="email" class="form-control" name="forget_pwd_email" id="email" required>
+                        </div>
+                        <div class="col-12">
+                            <button type="submit" class="btn btn-primary w-100" name="submit" id="login-btn">Submit</button>
+                        </div>
+                    </div>
+                </form>
                 <div class="row">
-                    <div class="col-12 mb-3">
-                        <label for="email">Email</label>
-                        <input type="email" class="form-control" name="forget_pwd_email" id="email" required>
+                    <div class="col-12 mt-3 text-center">
+                        <p class="text-bold">Back to Login</p>
+                        <a href="login.php" class="btn btn-info btn-md w-100">Login</a>
                     </div>
-                    <div class="col-12">
-                        <button type="submit" class="btn btn-primary w-100" name="submit" id="login-btn">Submit</button>
-                    </div>
-                </div>
-            </form>
-            <div class="row">
-                <div class="col-12 mt-3 text-center">
-                    <p class="text-bold">Back to Login</p>
-                    <a href="login.php" class="btn btn-info btn-md w-100">Login</a>
                 </div>
             </div>
         </div>
     </div>
-</div>
 
 
 
-<!-- REQUIRED SCRIPTS -->
-<!-- jQuery -->
-<script src="<?=site_url?>plugins/jquery/jquery.min.js"></script>
-<!-- Bootstrap -->
-<script src="<?=site_url?>plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
-<!-- bs-custom-file-input -->
-<script src="<?=site_url?>plugins/bs-custom-file-input/bs-custom-file-input.min.js"></script>
-<!-- overlayScrollbars -->
-<script src="<?=site_url?>plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js"></script>
-<!-- AdminLTE App -->
-<script src="<?=site_url?>dist/js/adminlte.js"></script>
+    <!-- REQUIRED SCRIPTS -->
+    <!-- jQuery -->
+    <script src="<?= site_url ?>plugins/jquery/jquery.min.js"></script>
+    <!-- Bootstrap -->
+    <script src="<?= site_url ?>plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <!-- bs-custom-file-input -->
+    <script src="<?= site_url ?>plugins/bs-custom-file-input/bs-custom-file-input.min.js"></script>
+    <!-- overlayScrollbars -->
+    <script src="<?= site_url ?>plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js"></script>
+    <!-- AdminLTE App -->
+    <script src="<?= site_url ?>dist/js/adminlte.js"></script>
 
-<!-- PAGE PLUGINS -->
-<!-- jQuery Mapael -->
-<script src="<?=site_url?>plugins/jquery-mousewheel/jquery.mousewheel.js"></script>
-<script src="<?=site_url?>plugins/raphael/raphael.min.js"></script>
-<script src="<?=site_url?>plugins/jquery-mapael/jquery.mapael.min.js"></script>
-<script src="<?=site_url?>plugins/jquery-mapael/maps/usa_states.min.js"></script>
-<!-- ChartJS 
+    <!-- PAGE PLUGINS -->
+    <!-- jQuery Mapael -->
+    <script src="<?= site_url ?>plugins/jquery-mousewheel/jquery.mousewheel.js"></script>
+    <script src="<?= site_url ?>plugins/raphael/raphael.min.js"></script>
+    <script src="<?= site_url ?>plugins/jquery-mapael/jquery.mapael.min.js"></script>
+    <script src="<?= site_url ?>plugins/jquery-mapael/maps/usa_states.min.js"></script>
+    <!-- ChartJS 
 <script src="< ?=site_url?>plugins/chart.js/Chart.min.js"></script>
 -->
-<!-- DataTables  & Plugins -->
-<script src="<?=site_url?>plugins/datatables/jquery.dataTables.min.js"></script>
-<script src="<?=site_url?>plugins/datatables-bs4/js/dataTables.bootstrap4.min.js"></script>
-<script src="<?=site_url?>plugins/datatables-responsive/js/dataTables.responsive.min.js"></script>
-<script src="<?=site_url?>plugins/datatables-responsive/js/responsive.bootstrap4.min.js"></script>
-<script src="<?=site_url?>plugins/datatables-buttons/js/dataTables.buttons.min.js"></script>
-<script src="<?=site_url?>plugins/datatables-buttons/js/buttons.bootstrap4.min.js"></script>
-<script src="<?=site_url?>plugins/jszip/jszip.min.js"></script>
-<script src="<?=site_url?>plugins/pdfmake/pdfmake.min.js"></script>
-<script src="<?=site_url?>plugins/pdfmake/vfs_fonts.js"></script>
-<script src="<?=site_url?>plugins/datatables-buttons/js/buttons.html5.min.js"></script>
-<script src="<?=site_url?>plugins/datatables-buttons/js/buttons.print.min.js"></script>
-<script src="<?=site_url?>plugins/datatables-buttons/js/buttons.colVis.min.js"></script>
-<!-- InputMask -->
-<script src="<?=site_url?>plugins/moment/moment.min.js"></script>
-<script src="<?=site_url?>plugins/inputmask/jquery.inputmask.min.js"></script>
-<!-- dateRangePicker -->
-<script src="<?=site_url?>plugins/daterangepicker/daterangepicker.js"></script>
-<!-- AdminLTE for demo purposes -->
-<script src="<?=site_url?>dist/js/demo.js"></script>
-<!-- AdminLTE dashboard demo (This is only for demo purposes) 
+    <!-- DataTables  & Plugins -->
+    <script src="<?= site_url ?>plugins/datatables/jquery.dataTables.min.js"></script>
+    <script src="<?= site_url ?>plugins/datatables-bs4/js/dataTables.bootstrap4.min.js"></script>
+    <script src="<?= site_url ?>plugins/datatables-responsive/js/dataTables.responsive.min.js"></script>
+    <script src="<?= site_url ?>plugins/datatables-responsive/js/responsive.bootstrap4.min.js"></script>
+    <script src="<?= site_url ?>plugins/datatables-buttons/js/dataTables.buttons.min.js"></script>
+    <script src="<?= site_url ?>plugins/datatables-buttons/js/buttons.bootstrap4.min.js"></script>
+    <script src="<?= site_url ?>plugins/jszip/jszip.min.js"></script>
+    <script src="<?= site_url ?>plugins/pdfmake/pdfmake.min.js"></script>
+    <script src="<?= site_url ?>plugins/pdfmake/vfs_fonts.js"></script>
+    <script src="<?= site_url ?>plugins/datatables-buttons/js/buttons.html5.min.js"></script>
+    <script src="<?= site_url ?>plugins/datatables-buttons/js/buttons.print.min.js"></script>
+    <script src="<?= site_url ?>plugins/datatables-buttons/js/buttons.colVis.min.js"></script>
+    <!-- InputMask -->
+    <script src="<?= site_url ?>plugins/moment/moment.min.js"></script>
+    <script src="<?= site_url ?>plugins/inputmask/jquery.inputmask.min.js"></script>
+    <!-- dateRangePicker -->
+    <script src="<?= site_url ?>plugins/daterangepicker/daterangepicker.js"></script>
+    <!-- AdminLTE for demo purposes -->
+    <script src="<?= site_url ?>dist/js/demo.js"></script>
+    <!-- AdminLTE dashboard demo (This is only for demo purposes) 
 <script src="< ?=site_url?>dist/js/pages/dashboard2.js"></script>
 -->
 
 
-<script>
-    $(document).ready(function(){
-        // $('#login-form').on('submit', function(e){
-        //     e.preventDefault();
-            
-        // });
-    });
-</script>
+    <script>
+        $(document).ready(function() {
+            $('#reset-form').on('submit', function(e) {
+                e.preventDefault();
+                let formData = $(this).serialize();
+                $.ajax({
+                    url: 'submit_req.php',
+                    method: 'post',
+                    data: formData,
+                    beforeSend: function(res) {
+                        $('#response_msg').addClass('text-info').html('please wait...');
+                    },
+                    success: function(res) {
+                        let r = JSON.parse(res);
+                        $('#response_msg').removeClass('text-info').addClass(r.class_).html(r.msg);
+                        console.log(r.status);
+                        if (r.status == 'success') {
+                            setTimeout(() => {
+                                window.location.href = 'login.php';
+                            }, 1200);
+                        }
+                    }
+                })
+            });
+        });
+    </script>
 
 </body>
+
 </html>
--- submit_req.php
+++ submit_req.php
@@ -10,53 +10,55 @@ require './php_mailer/vendor/autoload.php';
 require_once './core/database.php';
 include_once './core/functions.php';
 
-if(isset($_POST['forget_pwd_email'])) {
+if (isset($_POST['forget_pwd_email'])) {
 
-$forgetPwdEmail = $_POST['forget_pwd_email'];
+    $forgetPwdEmail = $_POST['forget_pwd_email'];
 
-$checkEmailExist = checkEmailExist($forgetPwdEmail,$db);
+    $checkEmailExist = checkEmailExist($forgetPwdEmail, $db);
 
-if($checkEmailExist) {
-$pwd = rand(999999,9999999);
-$pwdMD5 = md5($pwd);
-$db->query("UPDATE `employees` SET `password`='$pwdMD5' WHERE `email`='$forgetPwdEmail'");
-//Create an instance; passing `true` enables exceptions
-$mail = new PHPMailer(true);
+    if ($checkEmailExist) {
+        $pwd = rand(999999, 9999999);
+        $pwdMD5 = md5($pwd);
+        $db->query("UPDATE `employees` SET `password`='$pwdMD5' WHERE `email`='$forgetPwdEmail'");
+        //Create an instance; passing `true` enables exceptions
+        $mail = new PHPMailer(true);
 
-try {
-    //Server settings
-    $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
-    $mail->isSMTP();                                            //Send using SMTP
-    $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
-    $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-    $mail->Username = 'arshidni2023@gmail.com';
-    $mail->Password = 'vtujprgdlrywzycz';
-    $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;            //Enable implicit TLS encryption
-    $mail->Port       = 587;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+        try {
+            //Server settings
+            $mail->SMTPDebug = SMTP::DEBUG_OFF;                      //Enable verbose debug output
+            $mail->isSMTP();                                            //Send using SMTP
+            $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
+            $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
+            $mail->Username = 'arshidni2023@gmail.com';
+            $mail->Password = 'vtujprgdlrywzycz';
+            $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;            //Enable implicit TLS encryption
+            $mail->Port       = 587;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
-    //Recipients
-    $mail->setFrom('info@professionalcoast.com', 'Mailer');
-    $mail->addAddress($forgetPwdEmail);     //Add a recipient
-    // $mail->addAddress('ellen@example.com');               //Name is optional
-    // $mail->addReplyTo('info@example.com', 'Information');
-    // $mail->addCC('cc@example.com');
-    // $mail->addBCC('bcc@example.com');
+            //Recipients
+            $mail->setFrom('info@professionalcoast.com', 'Mailer');
+            $mail->addAddress($forgetPwdEmail);     //Add a recipient
+            // $mail->addAddress('ellen@example.com');               //Name is optional
+            // $mail->addReplyTo('info@example.com', 'Information');
+            // $mail->addCC('cc@example.com');
+            // $mail->addBCC('bcc@example.com');
 
-    //Attachments
-    // $mail->addAttachment('/var/tmp/file.tar.gz');         //Add attachments
-    // $mail->addAttachment('/tmp/image.jpg', 'new.jpg');    //Optional name
+            //Attachments
+            // $mail->addAttachment('/var/tmp/file.tar.gz');         //Add attachments
+            // $mail->addAttachment('/tmp/image.jpg', 'new.jpg');    //Optional name
 
-    //Content
-    $mail->isHTML(true);                                  //Set email format to HTML
-    $mail->Subject = 'Professional Costs | Password Reset';
-    $mail->Body    = 'This is your New Password <strong>'.$pwd.'</strong>';
-    $mail->AltBody = '<strong>Hint:</strong> Note down your passwords in your notebook that you think is safe from the reach of others.';
+            //Content
+            $mail->isHTML(true);                                  //Set email format to HTML
+            $mail->Subject = 'Professional Costs | Password Reset';
+            $mail->Body    = 'This is your New Password <strong>' . $pwd . '</strong>';
+            $mail->AltBody = '<strong>Hint:</strong> Note down your passwords in your notebook that you think is safe from the reach of others.';
 
-    $mail->send();
-    echo 'Message has been sent';
-} catch (Exception $e) {
-    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
-}
-}
-
+            $mail->send();
+            echo json_encode(['class_' => 'text-success', 'msg' => 'Password Request has been sent.', 'status' => 'success']);
+        } catch (Exception $e) {
+            echo json_encode(['class_' => 'text-danger', 'msg' => $mail->ErrorInfo, 'status' => 'error']);
+            // echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+        }
+    } else {
+        echo json_encode(['class_' => 'text-info', 'msg' => 'Email not in our database.', 'status' => 'error']);
+    }
 }
