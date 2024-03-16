ba85b167335b2d65bc9c1fdd1516d49872bfc871
SaadiDK-003 <pcremote059@gmail.com>
2024-03-16 16:43:36 +0500

    db file update

--- core/database.php
+++ core/database.php
@@ -4,8 +4,16 @@ $host = $_SERVER['HTTP_HOST'];
 if ($host == 'localhost') {
     $dirPath = 'professional_costs';
 } else {
-    $dirPath = 'panel';
+    $dirPath = 'professional_costs';
 }
+$data = '';
+$username = '';
+$email = '';
+$contact = '';
+$designation = '';
+$status = '';
+$role = '';
+$avatar = '';
 require_once $_SERVER['DOCUMENT_ROOT'].'/'.$dirPath.'/config.php';
 require_once 'functions.php';
 $db = mysqli_connect(HOST,USER,PWD,DB);
